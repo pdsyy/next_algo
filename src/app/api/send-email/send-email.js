89967c8d870c5b9f047f3128fea3bb38d2c05f62
pdsyy <prodseoy@gmail.com>
2026-04-21 19:29:37 +0300

    pre final1,0

---
+++ src/app/api/send-email/send-email.js
@@ -0,0 +1,27 @@
+import { Resend } from 'resend';
+import { NextResponse } from 'next/server';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req) {
+    try {
+        const dataF = await req.json();
+
+        const data = await resend.emails.send({
+            from: 'info@algo-bots.com',
+            to: 'mailvladimirab@gmail.com',
+            subject: 'Новая заявка с сайта ALGO',
+            html: `
+                <h2>Новая заявка с сайта ALGO WEB</h2>
+                <p><strong>Название бота:</strong> ${dataF.botName}</p>
+                <p><strong>Связь через:</strong> ${dataF.contactChanel}</p>
+                <p><strong>Имя:</strong> ${dataF.userName}</p>
+                <p><strong>Аккаунт или почта:</strong> ${dataF.account}</p>
+            `
+        });
+
+        return NextResponse.json({ success: true, data });
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}

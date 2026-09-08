6789400d582abfc65893814b17fec2a61e3ad5af
pdsyy <prodseoy@gmail.com>
2026-09-08 17:27:25 +0300

    check 08.09 v1.7

--- src/components/cartPopup/AddToCartButton.tsx
+++ src/components/cartPopup/AddToCartButton.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import type { ReactNode } from "react";
-
+import {motion} from "framer-motion";
 import { type AddToCartItem, useCart } from "./CartProvider";
+import {HTMLMotionProps} from "framer-motion";
 
 type AddToCartButtonProps = {
     product: AddToCartItem;
@@ -19,9 +20,23 @@ export default function AddToCartButton({product, children, className, openAfter
         if (openAfterAdd) openCart();
     };
 
+    const fastEase = [0.25, 0.1, 0.25, 1.0];
+
+
+    const baseTransition: any = {duration: 0.7, ease: fastEase};
+    const baseViewport = {once: true, amount: 0.1};
+
+    const fadeUp: HTMLMotionProps<any> = {
+        initial: {opacity: 0, y: 30, translateZ: 0},
+        whileInView: {opacity: 1, y: 0, translateZ: 0},
+        viewport: baseViewport,
+        transition: baseTransition
+    };
+
+
     return (
-        <button type="button" className={className} onClick={handleClick}>
+        <motion.button type="button" className={className} onClick={handleClick} {...fadeUp}>
             {children}
-        </button>
+        </motion.button>
     );
 }
